7c77b789edcefb59c4312f87ed37ca627ca2a8e4
Mohammed Fahad <fahadballari@gmail.com>
2026-01-20 21:05:52 +0530

    Add traffic light control logic in traffic_light.ino

---
+++ code/traffic_light.ino
@@ -0,0 +1,116 @@
+#define RED D1
+#define YELLOW D2
+#define BLUE D5
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  // RED light
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, LOW);
+  delay(5000);
+
+  // YELLOW light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(BLUE, LOW);
+  delay(2000);
+
+  // GREEN / BLUE light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, HIGH);
+  delay(5000);
+}
+#define RED D1
+#define YELLOW D2
+#define BLUE D5
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  // RED light
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, LOW);
+  delay(5000);
+
+  // YELLOW light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(BLUE, LOW);
+  delay(2000);
+
+  // GREEN / BLUE light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, HIGH);
+  delay(5000);
+}
+#define RED D1
+#define YELLOW D2
+#define BLUE D5
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  // RED light
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, LOW);
+  delay(5000);
+
+  // YELLOW light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(BLUE, LOW);
+  delay(2000);
+
+  // GREEN / BLUE light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, HIGH);
+  delay(5000);
+}
+#define RED D1
+#define YELLOW D2
+#define BLUE D5
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  // RED light
+  digitalWrite(RED, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, LOW);
+  delay(5000);
+
+  // YELLOW light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(BLUE, LOW);
+  delay(2000);
+
+  // GREEN / BLUE light
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(BLUE, HIGH);
+  delay(5000);
+}
